c2820c50294cdcd23dbc12a1757e09dfec430b23
Dave Haeffner <dave.haeffner@applitools.com>
2021-02-23 08:05:08 -0500

    SIDE Eyes: add iOS simulator support (#381)
    * side: update ui and device list to include ios simulators alongside chrome emulation
    
    * side: transform legacy selected device options to new schema
    
    * --amend
    
    * side: update emulation device list
    
    * side: update options parser to support new schema and support the ios simulator payload needed for the visual-grid-client
    
    * side: fix sim ids
    
    * side: remove id for sims since they are not used
    
    * side: use devices array provided in the function instead of requiring it externally
    
    * side: add device type in parens on selected device screen

--- packages/side-eyes/src/app/components/VisualGridSelectedOptions/index.jsx
+++ packages/side-eyes/src/app/components/VisualGridSelectedOptions/index.jsx
@@ -12,9 +12,15 @@ export default class VisualGridSelectedOptions extends React.Component {
     return (
       <div className="selected-options">
         {this.props.items.map(function(item) {
+          let itemName = typeof item === 'string' ? item : item.name
+          if (item.type) {
+            if (item.type === 'simulator') itemName = itemName += ' (iOS simulator)'
+            else if (item.type === 'emulator') itemName = itemName += ' (Chrome emulator)'
+          }
+          const itemKey = typeof item === 'string' ? itemName : `${itemName}-${item.type}`
           return (
-            <div className="option" key={item}>
-              <div className="option-text">{item}</div>
+            <div className="option" key={itemKey}>
+              <div className="option-text">{itemName}</div>
               <RemoveButton onClick={this.props.removeOption.bind(this, item)} />
             </div>
           )
