d14bc33c6fdeb900517cc34d1bce35dc003ad321
Tal Hayut <tal.hayut@applitools.com>
2021-05-12 08:56:18 +0300

    fix: fixing cypress tests

--- packages/eyes-cypress/test/unit/plugin/shouldSetGlobalHooks.test.js
+++ packages/eyes-cypress/test/unit/plugin/shouldSetGlobalHooks.test.js
@@ -4,10 +4,6 @@ const {expect} = require('chai');
 const shouldSetGlobalHooks = require('../../../src/plugin/shouldSetGlobalHooks');
 
 describe('shouldSetGlobalHooks', () => {
-  it('should return false if eyesLegacyHooks flag is set', () => {
-    expect(shouldSetGlobalHooks({eyesLegacyHooks: true})).to.be.false;
-  });
-
   it('should return true if version >= 6.2.0 and experimentalRunEvents flag is set', () => {
     expect(shouldSetGlobalHooks({version: '6.2.0', experimentalRunEvents: true})).to.be.true;
   });
@@ -16,17 +12,8 @@ describe('shouldSetGlobalHooks', () => {
     expect(shouldSetGlobalHooks({version: '6.2.0'})).to.be.false;
   });
 
-  it('should return false if version >= 6.2.0 but in interactive mode', () => {
-    expect(
-      shouldSetGlobalHooks({version: '6.2.0', experimentalRunEvents: true, isInteractive: true}),
-    ).to.be.false;
-  });
-
   it('should return true if version >= 6.7.0', () => {
     expect(shouldSetGlobalHooks({version: '6.7.0'})).to.be.true;
   });
 
-  it('should return false even if version >= 6.7.0 but in interactive mode', () => {
-    expect(shouldSetGlobalHooks({version: '6.7.0', isInteractive: true})).to.be.false;
-  });
 });
