d9a3328250143c1914b0618e160b53094622f5a1
kyrylo <kyrylo.onufriiev@applitools.com>
2021-07-02 15:12:25 +0300

    [api] fix types

--- packages/eyes-api/src/input/FloatingMatchSettings.ts
+++ packages/eyes-api/src/input/FloatingMatchSettings.ts
@@ -15,18 +15,19 @@ export class FloatingMatchSettingsData implements Required<FloatingMatchSettings
   private _settings: FloatingMatchSettings
 
   constructor(settings: FloatingMatchSettings)
+  constructor(region: Region)
   constructor(
     x: number,
     y: number,
     width: number,
     height: number,
-    maxUpOffset: number,
-    maxDownOffset: number,
-    maxLeftOffset: number,
-    maxRightOffset: number,
+    maxUpOffset?: number,
+    maxDownOffset?: number,
+    maxLeftOffset?: number,
+    maxRightOffset?: number,
   )
   constructor(
-    settingsOrX: FloatingMatchSettings | number,
+    settingsOrRegionOrX: FloatingMatchSettings | Region | number,
     y?: number,
     width?: number,
     height?: number,
@@ -35,16 +36,18 @@ export class FloatingMatchSettingsData implements Required<FloatingMatchSettings
     maxLeftOffset?: number,
     maxRightOffset?: number,
   ) {
-    if (utils.types.isNumber(settingsOrX)) {
+    if (utils.types.isNumber(settingsOrRegionOrX)) {
       return new FloatingMatchSettingsData({
-        region: {x: settingsOrX, y, width, height},
+        region: {x: settingsOrRegionOrX, y, width, height},
         maxUpOffset,
         maxDownOffset,
         maxLeftOffset,
         maxRightOffset,
       })
+    } else if (!utils.types.has(settingsOrRegionOrX, 'region')) {
+      return new FloatingMatchSettingsData({region: settingsOrRegionOrX})
     }
-    this.region = settingsOrX.region
+    this.region = settingsOrRegionOrX.region
     this.maxUpOffset = maxUpOffset
     this.maxDownOffset = maxDownOffset
     this.maxLeftOffset = maxLeftOffset
