1f0ca4d25f9a6cc9da09711fbdd72313726131af
Tal Hayut <tal.hayut@applitools.com>
2021-08-12 14:47:27 +0300

    fix: fixing stdout level to number

--- packages/eyes-storybook/src/chalkify.js
+++ packages/eyes-storybook/src/chalkify.js
@@ -1,4 +1,4 @@
 const Chalk = require('chalk').Instance;
-const level = process.env.STDOUT_LEVEL || 2;
+const level = Number(process.env.STDOUT_LEVEL || 2);
 const chalk = new Chalk({level});
 module.exports = chalk;
