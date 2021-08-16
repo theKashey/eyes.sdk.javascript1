fa4e3aab27fd2b506cd9b897b1cc4def61f73dd1
Sophie De Paula <sophie24587@gamil.com>
2021-08-16 13:04:25 -0700

    cypress, fix eslint issues

--- packages/eyes-cypress/src/plugin/runningTests.js
+++ packages/eyes-cypress/src/plugin/runningTests.js
@@ -9,15 +9,17 @@ const runningTests = {
     this.tests = [];
   },
   async abortTests() {
-    return Promise.all(this.tests.map(async (test) => {
-      if (test.closePromise) {
-        // this condition doesn't really happen in the code, but it's here for purity. This is used in handlers' batchStart in order to cleanup the state in case a zombie open was called.
-        await test.closePromise
-      } else {
-        await test.abort()
-      }
-    }))
-  }
+    return Promise.all(
+      this.tests.map(async test => {
+        if (test.closePromise) {
+          // this condition doesn't really happen in the code, but it's here for purity. This is used in handlers' batchStart in order to cleanup the state in case a zombie open was called.
+          await test.closePromise;
+        } else {
+          await test.abort();
+        }
+      }),
+    );
+  },
 };
 
 module.exports = runningTests;
