dcc23f5c40004e78d551c25f98a24c6132333fd1
kyrylo <kyrylo.onufriiev@applitools.com>
2021-10-04 16:03:50 +0300

    [browser-extension] no parallel testing

--- packages/eyes-browser-extension/.mocharc.cvg.js
+++ packages/eyes-browser-extension/.mocharc.cvg.js
@@ -11,7 +11,7 @@ module.exports = {
     // './node_modules/@applitools/sdk-shared/coverage-tests/custom/**/*.spec.js',
   ],
   parallel: true,
-  jobs: 15,
+  jobs: 1,
   timeout: 0,
   reporter: 'spec-xunit-file',
   grep: mochaGrep({tags}),
