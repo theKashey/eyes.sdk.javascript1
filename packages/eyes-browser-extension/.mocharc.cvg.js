496d48ff908c110bd23532cb6e1f62f0125dc61e
kyrylo <kyrylo.onufriiev@applitools.com>
2021-10-04 16:48:04 +0300

    [browser-extension] set mocha jobs to 10

--- packages/eyes-browser-extension/.mocharc.cvg.js
+++ packages/eyes-browser-extension/.mocharc.cvg.js
@@ -11,7 +11,7 @@ module.exports = {
     // './node_modules/@applitools/sdk-shared/coverage-tests/custom/**/*.spec.js',
   ],
   parallel: true,
-  jobs: 1,
+  jobs: 10,
   timeout: 0,
   reporter: 'spec-xunit-file',
   grep: mochaGrep({tags}),
