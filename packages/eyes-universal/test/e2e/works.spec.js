0b3d727d8b39faffa3b1173095da20949bfd6f34
kyrylo <kyrylo.onufriiev@applitools.com>
2021-12-28 22:20:05 +0200

    [universal] add test debug logging

--- packages/eyes-universal/test/e2e/works.spec.js
+++ packages/eyes-universal/test/e2e/works.spec.js
@@ -5,7 +5,7 @@ describe('works', () => {
   const suffixes = {darwin: 'macos', linux: 'linux', win32: 'win'}
   it('works', async () => {
     const server = spawn(`./bin/eyes-universal-${suffixes[process.platform]}`, {
-      stdio: ['ignore', 'inherit', 'ignore'],
+      stdio: 'inherit',
     })
     try {
       await new Promise((resolve, reject) => {
