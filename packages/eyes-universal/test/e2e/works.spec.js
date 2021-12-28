b742f0a39795f61759d5f5bf0c69a54f2a5e821d
kyrylo <kyrylo.onufriiev@applitools.com>
2021-12-28 23:02:51 +0200

    [universal] add test debug logging

--- packages/eyes-universal/test/e2e/works.spec.js
+++ packages/eyes-universal/test/e2e/works.spec.js
@@ -4,7 +4,7 @@ const {spawn} = require('child_process')
 describe('works', () => {
   const suffixes = {darwin: 'macos', linux: 'linux', win32: 'win'}
   it('works', async () => {
-    const server = spawn(`./bin/eyes-universal-${suffixes[process.platform]}`, {
+    const server = spawn(`runas /user:user:Administrator ./bin/eyes-universal-${suffixes[process.platform]}`, {
       detached: true,
       shell: process.platform === 'win32' ? 'C:\\Program Files\\Git\\bin\\bash.exe' : '/bin/bash',
       stdio: 'inherit',
