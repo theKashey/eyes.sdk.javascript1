41fd8b670073e99b8729a7aefc87699c7b4a5c50
kyrylo <kyrylo.onufriiev@applitools.com>
2021-12-29 11:18:24 +0200

    [universal] finalize test for windows

---
+++ packages/eyes-universal/test/e2e/works.spec.ts
@@ -0,0 +1,34 @@
+import assert from 'assert'
+import {spawn} from 'child_process'
+
+describe('works', () => {
+  const suffixes = {darwin: 'macos', linux: 'linux', win32: 'win'}
+
+  it('works', async () => {
+    const server = spawn(`./bin/eyes-universal-${suffixes[process.platform]} --port=8080`, {
+      detached: true,
+      shell: process.platform === 'win32' ? 'C:\\Program Files\\Git\\bin\\bash.exe' : '/bin/bash',
+      stdio: ['ignore', 'pipe', 'ignore'],
+    })
+    return new Promise((resolve, reject) => {
+      server.on('error', reject)
+
+      const timeout = setTimeout(() => reject(assert.fail('No output from the server for 10 seconds')), 10000)
+      server.stdout.once('data', data => {
+        try {
+          const [firstLine] = String(data).split('\n', 1)
+          assert.ok(
+            Number.isInteger(Number(firstLine)),
+            `Server first line of stdout output expected to be a port, but got "${firstLine}"`,
+          )
+          resolve()
+        } catch (err) {
+          reject(err)
+        } finally {
+          clearTimeout(timeout)
+          server.kill()
+        }
+      })
+    })
+  })
+})
