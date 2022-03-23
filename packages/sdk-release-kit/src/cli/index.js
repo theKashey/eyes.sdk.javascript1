8318f872d86b1629d20618f4632c34f11d792da2
Dave Haeffner <dhaeffner@gmail.com>
2022-03-23 14:56:28 -0400

    bongo: update console.log for `bongo log` to reflect new command name

--- packages/sdk-release-kit/src/cli/index.js
+++ packages/sdk-release-kit/src/cli/index.js
@@ -60,7 +60,7 @@ yargs
       const lower = lowerVersion || versions[versionsBack]
       const upper = upperVersion || versions[0]
 
-      console.log('bongo commit-log output')
+      console.log('bongo log output')
       console.log(`package: ${pkgName}`)
       if (!args.versionsBack) {
         console.log(
