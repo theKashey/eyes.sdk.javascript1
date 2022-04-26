c5780c1b063a5fe2bad04e24d33df2c4276bdff9
Dave Haeffner <dhaeffner@gmail.com>
2022-04-26 11:16:15 -0400

    snippets: fix lint

--- packages/snippets/src/browser/isElementScrollable.js
+++ packages/snippets/src/browser/isElementScrollable.js
@@ -2,8 +2,12 @@ const getElementComputedStyleProperties = require('./getElementComputedStyleProp
 
 function isElementScrollable([element] = []) {
   const p = getElementComputedStyleProperties([element, ['overflow-x', 'overflow-y', 'overflow']])
-  return ((p[0] === 'scroll' || p[0] === 'auto' || p[2] === 'hidden') && element.scrollWidth > element.clientWidth) ||
-    ((p[1] === 'scroll' || p[1] === 'auto' || p[2] === 'hidden') && element.scrollHeight > element.clientHeight)
+  return (
+    ((p[0] === 'scroll' || p[0] === 'auto' || p[2] === 'hidden') &&
+      element.scrollWidth > element.clientWidth) ||
+    ((p[1] === 'scroll' || p[1] === 'auto' || p[2] === 'hidden') &&
+      element.scrollHeight > element.clientHeight)
+  )
 }
 
 module.exports = isElementScrollable
