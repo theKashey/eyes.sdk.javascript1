7d7efc7d1640e62043936c4acc5fcb87c129c589
Chaim Aharonson <97834812+chaim-aharonson-applitools@users.noreply.github.com>
2022-05-16 16:12:17 +0300

    Update config.js (#905)

--- packages/eyes-testcafe/test/coverage/config.js
+++ packages/eyes-testcafe/test/coverage/config.js
@@ -5,8 +5,8 @@ module.exports = {
     'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/js/overrides.js',
     {
       'check region by selector with vg classic': {},
-      'check window with default fully with vg classic': {},
-      'check frame after manual switch to frame with vg classic': {},
+      'check window with default fully with vg': {config: {branchName: 'js-sdk'}},
+      'check frame after manual switch to frame with vg classic (@webdriver)': {skip: true},
       // 'check region by selector within shadow dom with vg': {skipEmit: true},
       'check region by element within shadow dom with vg': {skipEmit: true},
     },
