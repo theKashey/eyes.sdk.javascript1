fb57537a8e0df8d253026da87aa8edad32924f79
Chaim Aharonson <97834812+chaim-aharonson-applitools@users.noreply.github.com>
2022-05-16 11:31:37 +0300

    Update config.js (#901)
    * Update config.js

--- packages/eyes-testcafe/test/coverage/config.js
+++ packages/eyes-testcafe/test/coverage/config.js
@@ -3,15 +3,10 @@ module.exports = {
   template: './test/coverage/template.hbs',
   overrides: [
     'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/js/overrides.js',
-    'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/js/js-overrides.js',
-    ...(process.env.APPLITOOLS_TEST_REMOTE === 'eg'
-      ? ['https://raw.githubusercontent.com/applitools/sdk.coverage.tests/universal-sdk/eg.overrides.js']
-      : []),
     {
       'check region by selector with vg classic': {},
-      'check window with default fully with vg classic': {skipEmit: true},
+      'check window with default fully with vg classic': {},
       'check frame after manual switch to frame with vg classic': {},
-      'check frame after manual switch to frame with vg classic (@webdriver)': {skipEmit: true},
       // 'check region by selector within shadow dom with vg': {skipEmit: true},
       'check region by element within shadow dom with vg': {skipEmit: true},
     },
