5979bc7028cc3a7d00b42f04a905de218bced4a7
Chaim Aharonson <chaim.aharonson@applitools.com>
2022-06-13 17:04:31 +0300

    update change-log

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -1,9 +1,10 @@
 /* eslint {"@typescript-eslint/ban-types": ["error", {"types": {"Function": false}}]} */
 
 export function isNotDefined(value: any) {
-  return isNull(value) || isString(value)
-    ? value === '' || value.toLowerCase() === 'null' || value.toLowerCase() === 'undefined'
-    : false
+  return (
+    isNull(value) ||
+    (isString(value) ? value === '' || value.toLowerCase() === 'null' || value.toLowerCase() === 'undefined' : false)
+  )
 }
 
 export function isDefined(value: any) {
