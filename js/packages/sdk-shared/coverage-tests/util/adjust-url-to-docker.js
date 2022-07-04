6f0f2a95cf216465999624ce488425f3a463d8ba
kyrylo <kyrylo.onufriiev@applitools.com>
2022-07-04 20:31:05 +0300

    fix adjustUrlToDocker

--- js/packages/sdk-shared/coverage-tests/util/adjust-url-to-docker.js
+++ js/packages/sdk-shared/coverage-tests/util/adjust-url-to-docker.js
@@ -1,7 +1,7 @@
 // This is why we can't have nice things
 // https://github.com/docker/compose/issues/3800#issuecomment-496480571
 function adjustUrlToDocker(url, {platform = process.platform} = {}) {
-  if (platform === 'darwin') url.replace(/:\/\/localhost/, '://host.docker.internal')
+  if (platform === 'darwin') return url.replace(/:\/\/localhost/, '://host.docker.internal')
   return url
 }
 
