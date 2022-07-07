6c21e81d40ade5c6ee0a34fd39447b61ae3a12b5
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2022-07-07 16:45:46 +0300

    [py] move changelog extraction to send_mail.sh script

--- python/ci_scripts/send_mail.sh
+++ python/ci_scripts/send_mail.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-curl http://sdk-test-results.herokuapp.com/send_mail -X POST -H "Content-Type: application/json" -d "{\"sdk\":\"$1\", \"version\":\"$2\", \"changeLog\":\"$3\", \"testCoverageGap\":\"$4\"}"
+set -eu
+SDK_VERSION="$1"
+CHANGELOG_FILE="$2"
+CI_SCRIPTS=$(dirname "$0")
+CHANGELOG=$(bash "$CI_SCRIPTS/extract_changelog.sh" "${SDK_VERSION}" "$CHANGELOG_FILE")
+TEST_COVERAGE_GAP=$(cat "$CI_SCRIPTS/testCoverageGap.txt")
+curl http://sdk-test-results.herokuapp.com/send_mail -X POST -H "Content-Type: application/json" -d "{\"sdk\":\"python\", \"version\":\"$SDK_VERSION\", \"changeLog\":\"$CHANGELOG\", \"testCoverageGap\":\"$TEST_COVERAGE_GAP\"}"
