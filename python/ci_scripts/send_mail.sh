526af928029fc842bf55491af8448ec22b461e04
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2022-07-07 22:23:52 +0300

    [py] report eyes-robotframework published releases as "eyes-robotframework" sdk

--- python/ci_scripts/send_mail.sh
+++ python/ci_scripts/send_mail.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -eu
-SDK_VERSION="$1"
-CHANGELOG_FILE="$2"
+SDK_NAME="$1"
+SDK_VERSION="$2"
+CHANGELOG_FILE="$3"
 CI_SCRIPTS=$(dirname "$0")
 CHANGELOG=$(bash "$CI_SCRIPTS/extract_changelog.sh" "${SDK_VERSION}" "$CHANGELOG_FILE")
 TEST_COVERAGE_GAP=$(cat "$CI_SCRIPTS/testCoverageGap.txt")
-curl http://sdk-test-results.herokuapp.com/send_mail -X POST -H "Content-Type: application/json" -d "{\"sdk\":\"python\", \"version\":\"$SDK_VERSION\", \"changeLog\":\"$CHANGELOG\", \"testCoverageGap\":\"$TEST_COVERAGE_GAP\"}"
+curl http://sdk-test-results.herokuapp.com/send_mail -X POST -H "Content-Type: application/json" -d "{\"sdk\":\"$SDK_NAME\", \"version\":\"$SDK_VERSION\", \"changeLog\":\"$CHANGELOG\", \"testCoverageGap\":\"$TEST_COVERAGE_GAP\"}"
