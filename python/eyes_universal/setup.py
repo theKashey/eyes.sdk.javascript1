303b98c8ec9596a311112185880e0f4439f4bf27
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2022-07-26 20:25:19 +0300

    [py] skip re-downloading already downloaded executable with matching hash (#1049)

--- python/eyes_universal/setup.py
+++ python/eyes_universal/setup.py
@@ -1,4 +1,6 @@
-from os import chmod, mkdir, path, remove
+from base64 import b64encode
+from hashlib import md5
+from os import chmod, path, remove
 from sys import platform
 
 from setuptools import setup
@@ -6,16 +8,18 @@ from setuptools.command.build_py import build_py as _build_py
 from setuptools.command.sdist import sdist as _sdist
 
 try:
-    from urllib.request import urlretrieve
+    from urllib.request import HTTPError, Request, urlopen, urlretrieve
 except ImportError:
     from urllib import urlretrieve
+
+    from urllib2 import HTTPError, Request, urlopen
+
+    FileNotFoundError = IOError
 
 try:
     from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 except ImportError:
     _bdist_wheel = None
-
-commands = set()
 
 
 def current_platform_executable():
@@ -32,24 +36,56 @@ def current_platform_executable():
         raise Exception("Platform is not supported", platform)
 
 
-# Download previously selected executable.
-# It is downloaded to the source tree (so editable setup works). Added it to the
-# package_data of the distribution to be installed or packed into wheel.
-@commands.add
-class build_py(_build_py):  # noqa
-    def get_data_files(self):
-        if download_executable:
-            ext = ".exe" if download_executable == "win" else ""
-            version = self.distribution.get_version()
-            package_bin_name = "bin/eyes-universal" + ext
-            relative_bin_name = "applitools/eyes_universal/" + package_bin_name
-            url_template = (
-                "https://github.com/applitools/eyes.sdk.javascript1/releases/download/"
-                "%40applitools%2Feyes-universal%40{version}/eyes-universal-{exe}{ext}"
-            )
-            url = url_template.format(version=version, exe=download_executable, ext=ext)
-            if not path.isdir(path.dirname(relative_bin_name)):
-                mkdir(path.dirname(relative_bin_name))
+def platform_executable(plat):
+    if "macosx" in plat:
+        return "macos"
+    elif "manylinux" in plat:
+        return "linux"
+    elif "musllinux" in plat:
+        return "alpine"
+    elif "win" in plat:
+        return "win"
+    else:
+        raise ValueError("Unsupported platform", plat)
+
+
+def b64_encoded_file_md5(file_name):
+    try:
+        with open(file_name, "rb") as file:
+            hasher = md5()
+            for chunk in iter(lambda: file.read(4096), b""):
+                hasher.update(chunk)
+            return b64encode(hasher.digest()).decode("utf-8")
+    except FileNotFoundError:
+        return None
+
+
+def b64_encoded_url_md5(url):
+    head_request = Request(url)
+    head_request.get_method = lambda: "HEAD"
+    try:
+        response = urlopen(head_request)
+        return response.headers.get("Content-MD5", None)
+    except HTTPError:
+        return None
+
+
+def download(executable, version, dry_run):
+    ext = ".exe" if executable == "win" else ""
+    package_bin_name = "bin/eyes-universal" + ext
+    if not dry_run:
+        relative_bin_name = "applitools/eyes_universal/" + package_bin_name
+        url_template = (
+            "https://github.com/applitools/eyes.sdk.javascript1/releases/download/"
+            "%40applitools%2Feyes-universal%40{version}/eyes-universal-{exe}{ext}"
+        )
+        url = url_template.format(version=version, exe=executable, ext=ext)
+        existing_md5 = b64_encoded_file_md5(relative_bin_name)
+        if existing_md5 and existing_md5 == b64_encoded_url_md5(url):
+            already_downloaded = True
+        else:
+            already_downloaded = False
+        if not already_downloaded:
             try:
                 urlretrieve(url, relative_bin_name)
             except BaseException:
@@ -57,16 +93,20 @@ class build_py(_build_py):  # noqa
                     remove(relative_bin_name)
                 raise
             chmod(relative_bin_name, 0o755)
-            self.package_data[""].append(package_bin_name)
-        return _build_py.get_data_files(self)
+    return package_bin_name
+
+
+# By default all commands download current platform executable
+selected_executable = current_platform_executable()
+commands = set()
 
 
 # Prevent executable download and inclusion in source distributions.
 @commands.add
 class sdist(_sdist):  # noqa
     def finalize_options(self):
-        global download_executable
-        download_executable = None
+        global selected_executable
+        selected_executable = None
         return _sdist.finalize_options(self)
 
 
@@ -77,24 +117,29 @@ if _bdist_wheel:
     @commands.add
     class bdist_wheel(_bdist_wheel):  # noqa
         def finalize_options(self):
-            global download_executable
+            global selected_executable
             _bdist_wheel.finalize_options(self)
             # When plat-name argument is *not* provided to bdist_wheel, wheel is marked
             # with 'any' platform tag because it is also marked as "universal".
             # It has to be universal because it supports py2 and py3, but the executable
             # packaged is not cross-platform, so mark it with current platform's name.
             self.plat_name_supplied = True  # noqa
-            if "macosx" in self.plat_name:
-                download_executable = "macos"
-            elif "manylinux" in self.plat_name:
-                download_executable = "linux"
-            elif "musllinux" in self.plat_name:
-                download_executable = "alpine"
-            elif "win" in self.plat_name:
-                download_executable = "win"
-            else:
-                raise ValueError("Unsupported platform", self.plat_name)
+            selected_executable = platform_executable(self.plat_name)
 
 
-download_executable = current_platform_executable()
+# Download previously selected executable.
+# It is downloaded to the source tree (so editable setup works). Added it to the
+# package_data of the distribution to be installed or packed into wheel.
+@commands.add
+class build_py(_build_py):  # noqa
+    def get_data_files(self):
+        if selected_executable:
+            version = self.distribution.get_version()
+            self.mkpath("applitools/eyes_universal/bin")
+            self.announce("downloading %s executable" % selected_executable, 2)
+            downloaded_bin = download(selected_executable, version, self.dry_run)
+            self.package_data[""].append(downloaded_bin)
+        return _build_py.get_data_files(self)
+
+
 setup(cmdclass={c.__name__: c for c in commands}, package_data={"": []})
