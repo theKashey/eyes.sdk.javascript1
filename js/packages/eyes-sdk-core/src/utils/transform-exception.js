f4833a3697893566be9a6395a298de4debe17c1b
kyrylo <kyrylo.onufriiev@applitools.com>
2022-09-25 15:48:19 +0300

    [core-legacy] Fixed the wrong naming of the error info field `testResults` renamed to `testResult`

--- js/packages/eyes-sdk-core/src/utils/transform-exception.js
+++ js/packages/eyes-sdk-core/src/utils/transform-exception.js
@@ -1,6 +1,6 @@
 function transformException(error) {
   if (error.info) {
-    error.info = {...error.info, testResults: error.info.result, browserInfo: error.info.renderer}
+    error.info = {...error.info, testResult: error.info.result, browserInfo: error.info.renderer}
   }
   return error
 }
