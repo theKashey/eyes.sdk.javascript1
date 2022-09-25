ed5e6638261a9e4d6e36a346c0f7f735ac7653ab
kyrylo <kyrylo.onufriiev@applitools.com>
2022-09-25 17:47:39 +0300

    [core-legacy] Respond with dummy TestResult object on `Eyes.close` even if no session were actually started

--- js/packages/eyes-sdk-core/src/close.js
+++ js/packages/eyes-sdk-core/src/close.js
@@ -5,7 +5,25 @@ function makeClose({eyes, config: defaultConfig}) {
   return async function close({throwErr = false, config = defaultConfig} = {}) {
     try {
       const transformedConfig = transformConfig(config)
-      return await eyes.close({settings: {throwErr}, config: transformedConfig})
+      const results = await eyes.close({settings: {throwErr}, config: transformedConfig})
+      if (results.length > 0) return results
+      else {
+        return [
+          {
+            userTestId: eyes.test.userTestId,
+            name: '',
+            steps: 0,
+            matches: 0,
+            mismatches: 0,
+            missing: 0,
+            exactMatches: 0,
+            strictMatches: 0,
+            contentMatches: 0,
+            layoutMatches: 0,
+            noneMatches: 0,
+          },
+        ]
+      }
     } catch (error) {
       throw transformException(error)
     }
