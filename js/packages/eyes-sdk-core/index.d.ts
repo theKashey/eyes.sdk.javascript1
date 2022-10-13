c69c743e5cfa799bd21095f9b76ce452912ba007
kyrylo <kyrylo.onufriiev@applitools.com>
2022-10-13 17:48:40 +0300

    [core-legacy] fix types

--- js/packages/eyes-sdk-core/index.d.ts
+++ js/packages/eyes-sdk-core/index.d.ts
@@ -1,16 +1,17 @@
-import type * as types from '@applitools/types'
+import {Core} from '@applitools/core'
+import {SpecDriver} from '@applitools/driver'
 export {makeServer as makeExecutionGridClient} from '@applitools/execution-grid-client'
 
 export function makeSDK<TDriver, TContext, TElement, TSelector>(options: {
   name: string
   version: string
   cwd?: string
-  spec: types.SpecDriver<TDriver, TContext, TElement, TSelector>
-}): types.Core<TDriver, TElement, TSelector>
+  spec: SpecDriver<TDriver, TContext, TElement, TSelector>
+}): Core<TDriver, TElement, TSelector>
 
-export {Core} from '@applitools/types'
+export {Core} from '@applitools/core'
 
 export function checkSpecDriver<TDriver, TContext, TElement, TSelector>(options: {
-  spec: types.SpecDriver<TDriver, TContext, TElement, TSelector>,
+  spec: SpecDriver<TDriver, TContext, TElement, TSelector>,
   driver: TDriver
 }): Promise<any>
